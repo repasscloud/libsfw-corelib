c378834a81e6665675293223572f0c2bfe645b8f
danijeljw-rpc <danijel@repasscloud.com>
2022-01-16 01:09:27 +1100

    install npp for testing

--- lib/scripts/appveyor/02_build.ps1
+++ lib/scripts/appveyor/02_build.ps1
@@ -11,3 +11,7 @@ Get-ChildItem -Path $jsonMaps -Filter "*.ps1" | ForEach-Object {
     Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) Building JSON file for ingest: ${jsonFile}"
     & $_.FullName
 }
+
+# notepad++ for testing
+Invoke-WebRequest -UseBasicParsing -Uri https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2/npp.8.2.Installer.x64.exe -OutFile $env:TMP\npp.exe
+Start-Process -FilePath $env:TMP\npp.exe -ArgumentList '/S' -Wait
