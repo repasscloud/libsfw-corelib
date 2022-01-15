7a1ef1a1925efd71aa1c4a32537a557fc1ec4919
danijeljw-rpc <danijel@repasscloud.com>
2022-01-15 23:09:16 +1100

    adobe install verifiy

---
+++ build.ps1
@@ -0,0 +1,30 @@
+[System.String[]]$JsonFiles = Get-ChildItem -Path $PSScriptRoot\apps -Recurse -Filter "*.json" -File | Select-Object -ExpandProperty FullName
+
+foreach ($json in $JsonFiles)
+{
+    $j = Get-Content -Path $json | ConvertFrom-Json
+
+    [System.String]$Publisher = $j.id.publisher
+    [System.String]$Name = $j.id.name
+    [System.String]$Version = $j.id.version
+
+    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) Testing install for: ${Publisher} ${Name} ${Version}"
+
+    [System.String]$Arch = $j.id.arch
+    
+    $instObj = $j.id.installers.$Arch
+
+    Invoke-WebRequest -Uri $instObj.followuri -OutFile $env:TEMP\$instObj.filename
+
+    switch ($instObj.type)
+    {
+        'exe'
+        {
+            Start-Process -FilePath $env:TEMP\$instObj.filename -ArgumentList $instObj.switches -Wait
+        }
+        Default
+        {
+            Write-Output "Unknown file type"
+        }
+    }
+}
--- lib/scripts/init.ps1
+++
@@ -1,2 +0,0 @@
-Start-process -FilePath msiexec -ArgumentList '/X','{177B605A-B1E1-3197-B5D4-05F00C0174D1}','/q' -Wait
-Start-Process -FilePath "C:\Program Files (x86)\Mozilla Maintenance Service\uninstall.exe" -ArgumentList '/S' -Wait
