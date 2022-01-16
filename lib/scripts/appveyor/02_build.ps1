b34819f77fd6f7d60bac9a7ea4397e6ff8969ada
danijeljw-rpc <danijel@repasscloud.com>
2022-01-16 13:02:26 +1100

    code cleanup

--- lib/scripts/appveyor/02_build.ps1
+++ lib/scripts/appveyor/02_build.ps1
@@ -3,7 +3,7 @@
 [System.String]$jsonMaps = Join-Path -Path $rootPath -ChildPath "data\json-maps"
 
 # notice
-Write-output "-> Building JSON Library Files"
+Write-output ">>> Building JSON Library Files"
 
 # build all json maps
 Get-ChildItem -Path $jsonMaps -Filter "*.ps1" | ForEach-Object {
@@ -11,7 +11,3 @@ Get-ChildItem -Path $jsonMaps -Filter "*.ps1" | ForEach-Object {
     Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) Building JSON file for ingest: ${jsonFile}"
     & $_.FullName
 }
-
-# notepad++ for testing
-Invoke-WebRequest -Uri https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.2/npp.8.2.Installer.x64.exe -OutFile $env:TMP\npp.exe
-#Start-Process -FilePath $env:TMP\npp.exe -ArgumentList '/S' -Wait
