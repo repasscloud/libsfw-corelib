3b1e6b4e0dadbecc55902c853b359507b50f449d
danijeljw-rpc <danijel@repasscloud.com>
2022-01-17 00:48:19 +1100

    test build 20220117004819

--- lib/scripts/appveyor/03_exec.ps1
+++ lib/scripts/appveyor/03_exec.ps1
@@ -328,5 +328,5 @@ foreach ($jsonFile in $jsonFiles)
     {
         Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) BUCKET EXISTS: ${s3repo}/apps"
     }
-    Start-Process -FilePath mc -ArgumentList "cp","'${download_path}'","'${path}'" -Wait -ErrorAction Stop
+    Start-Process -FilePath mc -ArgumentList "cp","'${download_path}'","'${s3repo}/${path}'" -Wait -ErrorAction Stop
 }
