1f510b6e12edbe51f6bdbf92e00c1b7166c0f6e0
danijeljw-rpc <danijel@repasscloud.com>
2022-01-24 00:29:47 +1100

    stash test

--- lib/ci-scripts/appveyor/04_deploy.ps1
+++ lib/ci-scripts/appveyor/04_deploy.ps1
@@ -132,94 +132,83 @@ foreach ($jsonFile in $jsonFiles)
     # set detect value
     [System.String]$detect_value = $reg_src.PSPath.Replace("Microsoft.PowerShell.Core\Registry::","")
 
-    # uninstall application
-    Uninstall-ApplicationPackage -UninstallClass $uninstaller_class -UninstallString $uninstallstring
-
-
-    # verify uninstalled
-    if ($null -notlike (Get-ChildItem -Path $hklmPaths | Get-ItemProperty | Where-Object -FilterScript {$_.DisplayName -like $displayname}))
-    {
-        Get-ChildItem -Path $hklmPaths | Get-ItemProperty | Where-Object -FilterScript {$_.DisplayName -like $displayname}        
-    }
-    else
-    {
-        Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${displayname}"
-    }
+    # uninstall application (also verifies)
+    Uninstall-ApplicationPackage -UninstallClass $uninstaller_class -UninstallString $uninstallstring -DisplayName $displayname
 
     # write data to sql library
-    try
-    {
-        $UpdateAppsDBOutput = & $dll --uid $uid --key $app --latest --publisher $publisher --name $name --version $version `
-            --category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri `
-            --install-switches $switches --display-name $displayname --display-version $displayversion `
-            --display-publisher $displaypublisher --uninstall-string $uninstallstring `
-            --detect-method $detect_method --detect-value $detect_value --installer-class $installer_class --path $path `
-            --homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags `
-            --summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid `
-            --csHost $env:APPS_CS_HOST --csDB apps_db --csUsr $env:APPS_CS_USR --csPK $env:APPS_CS_PK
+#     try
+#     {
+#         $UpdateAppsDBOutput = & $dll --uid $uid --key $app --latest --publisher $publisher --name $name --version $version `
+#             --category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri `
+#             --install-switches $switches --display-name $displayname --display-version $displayversion `
+#             --display-publisher $displaypublisher --uninstall-string $uninstallstring `
+#             --detect-method $detect_method --detect-value $detect_value --installer-class $installer_class --path $path `
+#             --homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags `
+#             --summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid `
+#             --csHost $env:APPS_CS_HOST --csDB apps_db --csUsr $env:APPS_CS_USR --csPK $env:APPS_CS_PK
         
-        switch ($UpdateAppsDBOutput)
-        {
-            0
-            {
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) RECORD CREATED: ${uid}"
-                if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
-            }
-            1
-            {
-                Write-Output "$([char]::ConvertFromUTF32("0x1F7E0")) RECORD EXISTS: ${uid}"
-                #& $cni $env:GH_API $env:GH_USER $env:GH_REPO "record exists999" "this was created by api"
-                if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
-            }
-            Default
-            {
-                Write-Output "$([char]::ConvertFromUTF32("0x1F534")) UNHANDLED EXCEPTION: ${uid}"
-                if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
-                [System.String]$timeStamp = (Get-Date).ToString("yyyy-MM-dd HH:mm")
-                [System.String]$errorBodyText = @"
-Payload:
-``````powershell
---uid $uid --key $app --latest --publisher $publisher --name $name --version $version
---category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri
---install-switches $switches --display-name $displayname --display-version $displayversion
---display-publisher $displaypublisher --uninstall-string $uninstallstring
---detect-method $detect_method --detect-value $detect_value --installer-class $installer_class --path $path
---homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags
---summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid
-``````
-TimeStamp: ${timeStamp}
-"@
-                #& $cni $env:GH_API $env:GH_USER $env:GH_REPO "tbl_appdata ingest error: ${uid}" $errorBodyText
-                break  # break the loop on this item and move to the next one
-            }
-        }
-    }
-    catch
-    {
-        Write-Output "$([System.Char]::ConvertFromUtf32("0x1F534")) UNABLE TO CREATE SQL RECORD: ${app}"
-        if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
-    }
+#         switch ($UpdateAppsDBOutput)
+#         {
+#             0
+#             {
+#                 Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) RECORD CREATED: ${uid}"
+#                 if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
+#             }
+#             1
+#             {
+#                 Write-Output "$([char]::ConvertFromUTF32("0x1F7E0")) RECORD EXISTS: ${uid}"
+#                 #& $cni $env:GH_API $env:GH_USER $env:GH_REPO "record exists999" "this was created by api"
+#                 if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
+#             }
+#             Default
+#             {
+#                 Write-Output "$([char]::ConvertFromUTF32("0x1F534")) UNHANDLED EXCEPTION: ${uid}"
+#                 if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
+#                 [System.String]$timeStamp = (Get-Date).ToString("yyyy-MM-dd HH:mm")
+#                 [System.String]$errorBodyText = @"
+# Payload:
+# ``````powershell
+# --uid $uid --key $app --latest --publisher $publisher --name $name --version $version
+# --category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri
+# --install-switches $switches --display-name $displayname --display-version $displayversion
+# --display-publisher $displaypublisher --uninstall-string $uninstallstring
+# --detect-method $detect_method --detect-value $detect_value --installer-class $installer_class --path $path
+# --homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags
+# --summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid
+# ``````
+# TimeStamp: ${timeStamp}
+# "@
+#                 #& $cni $env:GH_API $env:GH_USER $env:GH_REPO "tbl_appdata ingest error: ${uid}" $errorBodyText
+#                 break  # break the loop on this item and move to the next one
+#             }
+#         }
+#     }
+#     catch
+#     {
+#         Write-Output "$([System.Char]::ConvertFromUtf32("0x1F534")) UNABLE TO CREATE SQL RECORD: ${app}"
+#         if (Test-Path -Path $download_path) { Remove-Item -Path $download_path -Confirm:$false -Force }
+#     }
 
-    <# VERBOSE TEST #>
-    "--uid $uid --key $app --latest --publisher $publisher --name $name --version $version " +
-    "--category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri " +
-    "--install-switches $switches --display-name $displayname --display-version $displayversion " +
-    "--display-publisher $displaypublisher --uninstall-string $uninstallstring " +
-    "--detect-method $detect_method --detect-value $detect_value --installer-class $uninstaller_class --path $path " +
-    "--homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags " +
-    "--summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid " +
-    " --csHost $env:APPS_CS_HOST --csDB apps_db --csUsr $env:APPS_CS_USR --csPK $env:APPS_CS_PK"
-    <# END VERBOSE TEST #>
+#     <# VERBOSE TEST #>
+#     "--uid $uid --key $app --latest --publisher $publisher --name $name --version $version " +
+#     "--category $category --arch $arch --exec-type $type --filename $filename --sha256 $shahash --follow-uri $followuri " +
+#     "--install-switches $switches --display-name $displayname --display-version $displayversion " +
+#     "--display-publisher $displaypublisher --uninstall-string $uninstallstring " +
+#     "--detect-method $detect_method --detect-value $detect_value --installer-class $uninstaller_class --path $path " +
+#     "--homepage $homepage --icon $icon --copyright $copyright --license $license --docs $docs --tags $tags " +
+#     "--summary $summary --rebootrequired $rebootrequired --depends $depends --lcid $lcid " +
+#     " --csHost $env:APPS_CS_HOST --csDB apps_db --csUsr $env:APPS_CS_USR --csPK $env:APPS_CS_PK"
+#     <# END VERBOSE TEST #>
 
-    # upload to S3
-    try
-    {
-        Start-Process -FilePath mc -ArgumentList "mb","${s3repo}/apps" -Wait -ErrorAction Stop
-        Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) BUCKET CREATED: ${s3repo}/apps"
-    }
-    catch
-    {
-        Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) BUCKET EXISTS: ${s3repo}/apps"
-    }
-    mc cp "${download_path}" "${s3repo}/${path}"
+#     # upload to S3
+#     try
+#     {
+#         Start-Process -FilePath mc -ArgumentList "mb","${s3repo}/apps" -Wait -ErrorAction Stop
+#         Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) BUCKET CREATED: ${s3repo}/apps"
+#     }
+#     catch
+#     {
+#         Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) BUCKET EXISTS: ${s3repo}/apps"
+#     }
+#     mc cp "${download_path}" "${s3repo}/${path}"
 }
--- lib/functions/Uninstall-ApplicationPackage.ps1
+++ lib/functions/Uninstall-ApplicationPackage.ps1
@@ -2,32 +2,57 @@ function Uninstall-ApplicationPackage {
     [CmdletBinding()]
     param (
         [System.String]$UninstallClass,
-        [System.String]$UninstallString
+        [System.String]$UninstallString,
+        [System.String]$DisplayName
     )
     
-    switch ($uninstaller_class)
-    {
-        'msi' {
-            if ($uninstallstring -match 'MsiExec.exe /I.*') {$uarg = $uninstallstring.Replace('MsiExec.exe /I', '') }
-            if ($uninstallstring -match 'MsiExec.exe /X.*') {$uarg = $uninstallstring.Replace('MsiExec.exe /X', '') }
-            try {
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) START UNINSTALL: ${displayname}"
-                Start-Process -FilePath msiexec -ArgumentList "/X","${uarg}","/passive" -Wait -ErrorAction Stop
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${displayname}"
+    begin {
+        [System.Array]$hklmPaths = @(
+            'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall',
+            'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall'
+        )
+    }
+
+    process {
+        switch ($uninstaller_class)
+        {
+            'msi' {
+                if ($uninstallstring -match 'MsiExec.exe /I.*') {$uarg = $uninstallstring.Replace('MsiExec.exe /I', '') }
+                if ($uninstallstring -match 'MsiExec.exe /X.*') {$uarg = $uninstallstring.Replace('MsiExec.exe /X', '') }
+                try {
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) START UNINSTALL: ${DisplayName}"
+                    Start-Process -FilePath msiexec -ArgumentList "/X","${uarg}","/q" -Wait -ErrorAction Stop
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${DisplayName}"
+                }
+                catch {
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) DID NOT UNINSTALL: ${DisplayName}"
+                }
             }
-            catch {
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) DID NOT UNINSTALL: ${displayname}"
+            'exe' {
+                try {
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) START UNINSTALL: ${DisplayName}"
+                    Start-Process -FilePath $uninstallstring -ArgumentList "${switches}" -Wait -ErrorAction Stop
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${DisplayName}"
+                }
+                catch {
+                    Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) DID NOT UNINSTALL: ${DisplayName}"
+                }
             }
         }
-        'exe' {
-            try {
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E1")) START UNINSTALL: ${displayname}"
-                Start-Process -FilePath $uninstallstring -ArgumentList "${switches}" -Wait -ErrorAction Stop
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${displayname}"
-            }
-            catch {
-                Write-Output "$([System.Char]::ConvertFromUTF32("0x1F534")) DID NOT UNINSTALL: ${displayname}"
-            }
+    
+        # verify app uninstalled
+        if ($null -notlike (Get-ChildItem -Path $hklmPaths | Get-ItemProperty | Where-Object -FilterScript {$_.DisplayName -like $DisplayName}))
+        {
+            Get-ChildItem -Path $hklmPaths | Get-ItemProperty | Where-Object -FilterScript {$_.DisplayName -like $DisplayName}
+            exit 1
         }
+        else
+        {
+            Write-Output "$([System.Char]::ConvertFromUTF32("0x1F7E2")) UNINSTALLED: ${DisplayName}"
+        }
+    }
+    
+    end {
+        [System.GC]::Collect()
     }
 }
