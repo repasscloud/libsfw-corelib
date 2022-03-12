8bc45478587c5c2093761c6a24f1b381826cf690
danijeljw-rpc <danijel@repasscloud.com>
2022-03-12 23:22:40 +1100

    fix adobe acrobat reader x64 path

--- adobe/adobe.readerdc.x64.exe.ps1
+++ adobe/adobe.readerdc.x64.exe.ps1
@@ -18,7 +18,7 @@ $adr_locale = "au-syd1-07"
 $adr_arch = "x64"
 $adr_lcid = "MUI"
 $adr_exectype = "exe"
-$adr_followuri = "https://ardownload2.adobe.com/pub/adobe/reader/win/AcrobatDC/" + $adr_version.Replace('.','') + "/AcroRdrDCx64" + $adr_version.Replace('.','') + "_MUI.exe"
+$adr_followuri = "https://ardownload2.adobe.com/pub/adobe/acrobat/win/AcrobatDC/" + $adr_version.Replace('.','') + "/AcroRdrDCx64" + $adr_version.Replace('.','') + "_MUI.exe"
 $adr_installswitches = "/sAll /msi /qn ALLUSERS=1 EULA_ACCEPT=YES DISABLEDESKTOPSHORTCUT=1"  # used for backwards compatability
 $adr_displayname = "Adobe Acrobat Reader DC MUI"  # OPTIONAL
 $adr_geo = "au-syd1-07"
