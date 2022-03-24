362162f16206bc0012a8c8acc94aca4a59db7836
danijeljw-rpc <danijel@repasscloud.com>
2022-03-24 19:31:01 +1100

    quick stash 20220324193101

---
+++ public/build_script.ps1
@@ -0,0 +1,5 @@
+[System.String[]]$SourceFiles = Get-ChildItem -Path .\data\corelib\ -Filter "*.ps1" -File -Recurse | Select-Object -ExpandProperty FullName
+foreach ($sf in $SourceFiles)
+{
+    $sf
+}
