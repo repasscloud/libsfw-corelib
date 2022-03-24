232993499579243521e93edd6fba8abc6fa95812
danijeljw-rpc <danijel@repasscloud.com>
2022-03-24 23:14:26 +1100

    quick stash 20220324231426

--- public/build_script.ps1
+++ public/build_script.ps1
@@ -1,6 +1,7 @@
 <# LOAD FUNCTIONS #>
 Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER\libsfw-ps -Filter "Export-JsonManifest.ps1" -File | ForEach-Object { . $_.FullName }
 Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER\libsfw-ps -Filter "Get-AbsoluteUri.ps1" -File | ForEach-Object { . $_.FullName }
+Get-ChildItem -Path $env:APPVEYOR_BUILD_FOLDER\libsfw-ps -Filter "New-GitHubIssue.ps1" -File | ForEach-Object { . $_.FullName }
 
 <# SOURCE CORELIB FILES #>
 [System.String[]]$SourceFiles = Get-ChildItem -Path .\data\corelib\ -Filter "*.ps1" -File -Recurse | Select-Object -ExpandProperty FullName
