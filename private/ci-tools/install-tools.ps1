42da410270b1df0d35766d660f53edcf7cc26868
danijeljw-rpc <danijel@repasscloud.com>
2022-03-27 17:20:03 +1100

    comments

--- private/ci-tools/install-tools.ps1
+++ private/ci-tools/install-tools.ps1
@@ -1,4 +1,4 @@
-# get a copy of optechx.DownloadFile
+# install optechx.DownloadFile.dll (odf)
 dotnet restore C:\Projects\optechx.DownloadFile\optechx.DownloadFile\optechx.DownloadFile.csproj
 msbuild C:\Projects\optechx.DownloadFile\optechx.DownloadFile.sln /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /property:Configuration=Release
 New-Item -Path C:\odf -ItemType Directory -Confirm:$false -Force
