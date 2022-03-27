e0eb08d30cb2b00f7edd4142c9c4cec8531ea1a2
danijeljw-rpc <danijel@repasscloud.com>
2022-03-27 14:17:12 +1100

    update gitignore

--- private/ci-tools/install-tools.ps1
+++
@@ -1,5 +0,0 @@
-# get a copy of optechx.DownloadFile
-dotnet restore C:\Projects\optechx.DownloadFile\optechx.DownloadFile\optechx.DownloadFile.csproj
-msbuild C:\Projects\optechx.DownloadFile\optechx.DownloadFile.sln /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /property:Configuration=Release
-New-Item -Path C:\odf -ItemType Directory -Confirm:$false -Force
-Copy-Item -Path C:\Projects\optechx.DownloadFile\optechx.DownloadFile\bin\Release\netcoreapp2.2\* -Destination C:\odf\ -Recurse -Confirm:$false -Force
