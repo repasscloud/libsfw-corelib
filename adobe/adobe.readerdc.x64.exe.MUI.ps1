9d74ed434699fda4b3532eea967ff90d626ed692
danijeljw-rpc <danijel@repasscloud.com>
2022-04-03 12:41:50 +1000

    crossing the river styx

--- adobe/adobe.readerdc.x64.exe.MUI.ps1
+++ adobe/adobe.readerdc.x64.exe.MUI.ps1
@@ -31,6 +31,7 @@ $adr_uninstallprocess = "msi"  # "void_uninstall","msi","exe","exe2","inno","scr
 $adr_uninstallcmd = "MsiExec.exe /I{AC76BA86-1033-FF00-7760-BC15014EA700}"
 $adr_uninstallargs = "msi-void"
 
+
 <# NOTES #>
 #~ CoreLib, do not edit
 #~ Adobe installs as an EXE, uninstalls as an MSI
